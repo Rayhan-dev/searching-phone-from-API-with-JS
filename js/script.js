30041db9c249a25d2b53e5ed4becf7001dd2429c
Rayhan-dev <mohammodrayhan444@gmail.com>
2022-03-01 17:27:18 +0600

    details functionality done

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ const show_results = () => {
 }
 const veiwResults = data => {
     const cardsDiv = document.getElementById('result_cards');
+    cardsDiv.textContent = "";
     for (let phones of data) {
         const card = document.createElement('div');
         card.classList = "col";
@@ -15,7 +16,7 @@ const veiwResults = data => {
             <div class="card-body">
                 <h5 class="card-title">Name: ${phones.phone_name} </h5>
                 <p class="card-text">Brand: ${phones.brand} </p>
-                <button id="detailsBtn" onclick="show_details('${phones.slug}')" type="button" class="btn btn-primary">Show details</button>
+                <a href="#details_info" id="detailsBtn"  onclick="show_details('${phones.slug}')" type="button" class="btn btn-primary">Show details</a>
             </div>
         </div>
       `;
@@ -26,6 +27,51 @@ const veiwResults = data => {
 const show_details = (slug) => {
     fetch(`https://openapi.programming-hero.com/api/phone/${slug}`)
         .then(response => response.json())
-        .then(data => console.log(data.data));
+        .then(data => veiwDetails(data.data));
 }
-const veiwDetails = (data) => 
+const veiwDetails = (data) => { 
+    const detailsDiv = document.getElementById('details_info');
+    detailsDiv.textContent = '';
+    const phoneDetails = document.createElement('div');
+    phoneDetails.classList ="row g-0";
+    phoneDetails.innerHTML = `
+  
+            <div class="col-md-4 d-flex align-items-center ps-3">
+                <img src="${data.image} " class="img-fluid rounded-start" alt="...">
+            </div>
+            <div class="col-md-8 align-items-center">
+                <div class="card-body">
+                    <h4 class="card-title">${data.name}</h4>
+                    <h5 class="card-title">Main Features :</h5>
+                    <p class="card-text">Chipset: ${data.mainFeatures.chipSet}</p>
+                    <p class="card-text">display Size: ${data.mainFeatures.displaySize}</p>
+                    <p class="card-text">Chipset: ${data.mainFeatures.chipSet}</p>
+                    <p class="card-text">Memory: ${data.mainFeatures.memory}</p>
+                    <p class="card-text">Storage: ${data.mainFeatures.storage}</p>
+                    <h5 class="card-title">Sensors :</h5>
+                    <div id="sensor_div"> 
+                             
+                    </div>
+                    <h5 class="card-title">Others :</h5>
+                    <p class="card-text">Bluetooth: ${data.others.Bluetooth}</p>
+                    <p class="card-text">GPS: ${data.others.GPS}</p>
+                    <p class="card-text">NFC: ${data.others.NFC}</p>
+                    <p class="card-text">Radio: ${data.others.Radio}</p>
+                    <p class="card-text">USB: ${data.others.USB}</p>
+                    <p class="card-text">WLAN: ${data.others.WLAN}</p>
+                    <p class="card-text"><small class="text-muted">${data.releaseDate}</small></p>        
+                </div>
+            </div>
+    `;
+   
+    detailsDiv.appendChild(phoneDetails);
+    const sensors = data.mainFeatures.sensors;
+    for (sensorName of sensors) {
+        const sensorDiv = document.getElementById('sensor_div');
+        console.log(sensorDiv);
+        const sensorList = document.createElement('ul');
+        sensorList.innerHTML= `<li>${sensorName}</li>`;
+        sensorDiv.appendChild(sensorList);
+    }
+}
+ 
